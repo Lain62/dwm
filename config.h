fd1c068521282e9f75512a09f811172a8b82f62d
lain62 <laimadelain@gmail.com>
2025-06-20 16:53:16 +0700

    added cool-autostart

--- config.h
+++ config.h
@@ -23,6 +23,16 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"st", NULL,
+    "~/scripts/startup", NULL,
+    "dunst", NULL,
+    "nm-applet", NULL,
+    "pipewire", NULL,
+    "pipewire-pulse", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
