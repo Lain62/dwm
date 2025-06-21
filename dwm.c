f30b2fcb0c4cae32a8f23f105cd8105193e40ff3
lain62 <laimadelain@gmail.com>
2025-06-22 00:14:55 +0700

    I dont rememebre why i dit his

--- dwm.c
+++ dwm.c
@@ -2602,7 +2602,7 @@ int
 main(int argc, char *argv[])
 {
 	if (argc == 2 && !strcmp("-v", argv[1]))
-		die("dwm-"VERSION);
+		die("dwm-"VERSION" Lain62 Ver");
 	else if (argc != 1)
 		die("usage: dwm [-v]");
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
