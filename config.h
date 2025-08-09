5a44dd2b16e35a75b83288e8679b1f1de5abfc4e
lain62 <laimadelain@gmail.com>
2025-08-09 22:40:01 +0700

    Changed font size

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=16" };
+static const char *fonts[]          = { "Iosevka:size=14" };
 static const char dmenufont[]       = "Iosevka:size=12";
 static const char rosevu_black_darker[]  = "#1B2021";
 static const char rosevu_black[]         = "#30343F";
